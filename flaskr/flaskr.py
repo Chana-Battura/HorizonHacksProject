2e6454a484b5ef3eabf3507e40bdefced564f49e
Charan Battula <charanbattula@pop-os.localdomain>
2020-10-24 20:13:20 -0700

    Added all the extra work

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,24 +1,30 @@
 from flask import Flask, jsonify, request, render_template
-import external
+import external, script
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
+# @app.route('/hello', methods=['GET', 'POST'])
+# def hello():
 
-    # POST request
-    if request.method == 'POST':
-        print('Incoming..')
-        print(request.json)
-        external.add_db(request.json)
-          # parse as JSON
-        return 'OK', 200
+#     # POST request
+#     if request.method == 'POST':
+#         print('Incoming..')
+#         print(request.json)
+#         external.add_db(request.json)
+#           # parse as JSON
+#         return 'OK', 200
+#
+#     # GET request
+#     else:
+#         json_data = external.give_data()
+#         return json_data
+#          #serialize and use JSON headers
 
-    # GET request
-    else:
-        json_data = external.give_data()
-        return json_data
-         #serialize and use JSON headers
+# @app.route('/test', methods=['get', 'post'])
+# def test_page():
+#     if request.method == 'POST':
+#         print(request.form)
+#     return render_template('index.html')
 
-@app.route('/test')
-def test_page():
-    return render_template('index.html')
+@app.route('/register', methods=['get', 'post'])
+def register():
+    return render_template('register.html')
--- flaskr/static/main.js
+++ flaskr/static/main.js
@@ -1,31 +1,28 @@
-var phone = {
-    'name': 'iphone',
-    'age': 5
-};
-var jsonString = JSON.stringify(phone);
 
-var xhttp = new XMLHttpRequest();
-
-xhttp.open("GET", "/hello", true);
-xhttp.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-        data = JSON.parse(xhttp.responseText);
-        document.getElementById("demo").innerHTML = xhttp.responseText;
-        post_request();
-    }
-};
-xhttp.send();
-
-function post_request() {
+function get_request() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/hello", true);
-    xhttp.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+    xhttp.open("GET", "/hello", true);
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           console.log(xhttp.responseText)
-        };
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+            data = JSON.parse(xhttp.responseText);
+            document.getElementById("demo").innerHTML = xhttp.responseText;
+        }
     };
-    xhttp.send(jsonString);
-};
+    xhttp.send();
+}
+
+
+// function post_request(data) {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open("POST", "/test", true);
+//     xhttp.setRequestHeader('Content-Type', 'multipart/form-data; charset=utf-8');
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//            // Typical action to be performed when the document is ready:
+//         };
+//     };
+//     xhttp.send(data);
+// };
+get_request()
